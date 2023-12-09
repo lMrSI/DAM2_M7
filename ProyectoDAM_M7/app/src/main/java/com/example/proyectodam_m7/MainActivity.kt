8364f64f5d57e57055fb89d1038691f90e68813c
Your Name <you@example.com>
2023-12-09 22:37:07 +0100

    2 páginas

--- ProyectoDAM_M7/app/src/main/java/com/example/proyectodam_m7/MainActivity.kt
+++ ProyectoDAM_M7/app/src/main/java/com/example/proyectodam_m7/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.proyectodam_m7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button4 = findViewById<Button>(R.id.button4)
+        button4.setOnClickListener {
+            val intent = Intent(this, Guardados::class.java)
+            startActivity(intent)
+        }
+
     }
 }
