3b06243b32d5aac92e70d50a1e8b905c03086a8a
Your Name <you@example.com>
2023-12-10 20:06:23 +0100

    todas las páginas

---
+++ ProyectoDAM_M7/app/src/main/java/com/example/proyectodam_m7/Registro.java
@@ -0,0 +1,13 @@
+package com.example.proyectodam_m7;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Registro extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro);
+    }
+}
