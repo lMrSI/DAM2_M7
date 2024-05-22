86b1b921da67d3947032906f928eafaf466f2c0e
Your Name <you@example.com>
2024-05-22 11:52:44 +0200

    practica 10

--- AppM7/app/src/main/java/com/example/appm7/Guardar.kt
+++ AppM7/app/src/main/java/com/example/appm7/Guardar.kt
@@ -1,15 +1,21 @@
 package com.example.proyectodam_m7
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.appm7.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
 class Guardar : AppCompatActivity() {
-    lateinit var navegation : BottomNavigationView
+    lateinit var navegation: BottomNavigationView
+    lateinit var textView7: TextView
+
     private val mOnNavMenu = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when(item.itemId){
+        when (item.itemId) {
             R.id.primerFragment -> {
                 supportFragmentManager.beginTransaction().apply {
                     replace(R.id.fragmentContainer, PrimerFragment())
@@ -36,6 +42,7 @@ class Guardar : AppCompatActivity() {
                 }
                 return@OnNavigationItemSelectedListener true
             }
+
             R.id.cuartoFragment -> {
                 supportFragmentManager.beginTransaction().apply {
                     replace(R.id.fragmentContainer, CuartoFragment())
@@ -44,6 +51,7 @@ class Guardar : AppCompatActivity() {
                 }
                 return@OnNavigationItemSelectedListener true
             }
+
             R.id.quintoFragment -> {
                 supportFragmentManager.beginTransaction().apply {
                     replace(R.id.fragmentContainer, QuintoFragment())
@@ -52,13 +60,21 @@ class Guardar : AppCompatActivity() {
                 }
                 return@OnNavigationItemSelectedListener true
             }
-
         }
         false
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.guardados)
+        textView7.setOnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                Log.d("TOUCH_EVENT", "TextView touched")
+                textView7.showTooltip("Este es el menú de Guardado", event.rawX.toInt(), event.rawY.toInt())
+            }
+            true
+        }
+
         navegation = findViewById(R.id.nav_menu)
         navegation.setOnNavigationItemSelectedListener(mOnNavMenu)
 
@@ -67,5 +83,10 @@ class Guardar : AppCompatActivity() {
             addToBackStack("replacement")
             commit()
         }
+    }
+
+    private fun TextView.showTooltip(tooltipText: String, x: Int, y: Int) {
+        Log.d("TOOLTIP", "Showing tooltip: $tooltipText")
+        Toast.makeText(this@Guardar, tooltipText, Toast.LENGTH_SHORT).show()
     }
 }
--- AppM7/app/src/main/java/com/example/appm7/Inicio.kt
+++ AppM7/app/src/main/java/com/example/appm7/Inicio.kt
@@ -1,16 +1,26 @@
 package com.example.proyectodam_m7
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.TooltipCompat
 import com.example.appm7.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Inicio : AppCompatActivity() {
-    lateinit var navegation : BottomNavigationView
+    lateinit var navegation: BottomNavigationView
+    lateinit var textViewVerconnect: TextView
+    lateinit var imageView47: ImageView
+
     private val mOnNavMenu = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when(item.itemId){
+        when (item.itemId) {
             R.id.primerFragment -> {
                 supportFragmentManager.beginTransaction().apply {
                     replace(R.id.fragmentContainer, PrimerFragment())
@@ -37,6 +47,7 @@ class Inicio : AppCompatActivity() {
                 }
                 return@OnNavigationItemSelectedListener true
             }
+
             R.id.cuartoFragment -> {
                 supportFragmentManager.beginTransaction().apply {
                     replace(R.id.fragmentContainer, CuartoFragment())
@@ -45,6 +56,7 @@ class Inicio : AppCompatActivity() {
                 }
                 return@OnNavigationItemSelectedListener true
             }
+
             R.id.quintoFragment -> {
                 supportFragmentManager.beginTransaction().apply {
                     replace(R.id.fragmentContainer, QuintoFragment())
@@ -53,13 +65,32 @@ class Inicio : AppCompatActivity() {
                 }
                 return@OnNavigationItemSelectedListener true
             }
-
         }
         false
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.inicio)
+        textViewVerconnect = findViewById(R.id.textView8)
+        imageView47 = findViewById(R.id.imageView47)
+
+        if (Build.VERSION.SDK_INT > 25) {
+            textViewVerconnect.setOnTouchListener { v, event ->
+                if (event.action == MotionEvent.ACTION_DOWN) {
+                    textViewVerconnect.showTooltip("Clickea para la navegacuión entre plataformas", event.rawX.toInt(), event.rawY.toInt())
+                }
+                true
+            }
+
+            imageView47.setOnTouchListener { v, event ->
+                if (event.action == MotionEvent.ACTION_DOWN) {
+                    imageView47.showTooltip("Este es el menu de inicio", event.rawX.toInt(), event.rawY.toInt())
+                }
+                true
+            }
+        }
+
         navegation = findViewById(R.id.nav_menu)
         navegation.setOnNavigationItemSelectedListener(mOnNavMenu)
 
@@ -69,21 +100,32 @@ class Inicio : AppCompatActivity() {
             commit()
         }
     }
+
     fun onVideojuegosButtonClick(view: View) {
         val intent = Intent(this, Videojuegos::class.java)
         startActivity(intent)
     }
+
     fun onPeliculasButtonClick(view: View) {
         val intent = Intent(this, Peliculas::class.java)
         startActivity(intent)
     }
+
     fun onSeriesButtonClick(view: View) {
         val intent = Intent(this, Series::class.java)
         startActivity(intent)
     }
+
     fun onAnimeButtonClick(view: View) {
         val intent = Intent(this, Anime::class.java)
         startActivity(intent)
     }
-}
 
+    private fun TextView.showTooltip(tooltipText: String, x: Int, y: Int) {
+        Toast.makeText(this@Inicio, tooltipText, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun ImageView.showTooltip(tooltipText: String, x: Int, y: Int) {
+        Toast.makeText(this@Inicio, tooltipText, Toast.LENGTH_SHORT).show()
+    }
+}
